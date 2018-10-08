1d0f1a2a7649b4e08cf0eb7eddc9e42e9895df71
Kenia Rioja <kdrioja@gmail.com>
2018-10-08 15:56:48 -0700

    Added message sending functionality

--- ParseChat/ChatViewController.swift
+++ ParseChat/ChatViewController.swift
@@ -7,24 +7,33 @@
 //
 
 import UIKit
+import Parse
 
 class ChatViewController: UIViewController {
 
+    
+    @IBOutlet weak var messageTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func onSend(_ sender: Any) {
+        let chatMessage = PFObject(className: "Message")
+        chatMessage["text"] = messageTextField.text ?? ""
+        
+        chatMessage.saveInBackground { (success: Bool, error: Error?) in
+            if success {
+                print("The message was saved!")
+                self.messageTextField.text = ""
+            }
+            else if let error = error {
+                print("Problem saving message: \(error.localizedDescription)")
+            }
+        }
     }
-    */
-
+    
 }
