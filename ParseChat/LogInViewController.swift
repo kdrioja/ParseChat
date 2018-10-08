c7f3e501a152dfca8526f03838c0981df9edff66
Kenia Rioja <kdrioja@gmail.com>
2018-10-08 14:39:38 -0700

    set up parse

---
+++ ParseChat/LogInViewController.swift
@@ -0,0 +1,31 @@
+//
+//  LogInViewController.swift
+//  ParseChat
+//
+//  Created by user145766 on 10/8/18.
+//  Copyright © 2018 Kenia Rioja. All rights reserved.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var errorLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    
+    @IBAction func onSignUp(_ sender: Any) {
+    }
+    
+    @IBAction func onLogIn(_ sender: Any) {
+    }
+    
+
+}
